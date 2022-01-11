11d3c31dfad40e373b6e65680cbabedc59829730
Lee YoonJae <yjlee1362@gmail.com>
2022-01-11 17:51:09 +0900

    ver.1.0

---
+++ src/App.js
@@ -0,0 +1,97 @@
+import logo from './logo.svg';
+import { Button, Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import './App.css';
+import { useState } from 'react';
+import dett from './data.js';
+
+import {Link,Route,Switch} from 'react-router-dom';
+
+function App() {
+
+  let [shoes, shoes변경] = useState(dett);
+
+  return (
+    <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">PC샵</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+    
+      <Route  exact path="/">
+      <div className='jumbotron'>
+        안녕친구들 <br />
+        일단 이런거 해봤어<br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <div className='buttonIn'>
+          <Button variant="dark">확인</Button>{' '}
+        </div>
+      </div>
+      <div className='container'>
+        <div className='row'>
+          {shoes.map((a, i) => { return (<상품 key={i} shoes={shoes[i]} />) })}
+
+        </div>
+
+      </div>
+
+      </Route>
+      <Route path="/detail">
+      <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+        </div>
+        <div className="col-md-6 mt-4">
+          <h4 className="pt-5">상품명</h4>
+          <p>상품설명</p>
+          <p>120000원</p>
+          <button className="btn btn-danger">주문하기</button> 
+        </div>
+      </div>
+</div> 
+      </Route>
+      {/* <Route path="/어떤경로" component={컴포넌트이름}></Route> */}
+
+     
+
+
+
+    </div>
+
+  );
+}
+
+function 상품(props) {
+  return (
+    <div className="col-md-4" >
+      <img src={props.shoes.src} width="100%" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content}</p>
+      <p>{props.shoes.price}</p>
+    </div>
+
+  );
+}
+
+
+export default App;
