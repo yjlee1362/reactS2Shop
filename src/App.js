c3cf5f2cbc3c10ccbbddbec1906e8e0065071d6e
Lee YoonJae <yjlee1362@gmail.com>
2022-01-13 16:13:40 +0900

    ver.1.2

--- src/App.js
+++ src/App.js
@@ -5,11 +5,32 @@ import { useState } from 'react';
 import dett from './data.js';
 import Detail from './Detail.js';
 
-import { Link, Route, Switch } from 'react-router-dom';
+import { Link, Route, Switch, useHistory } from 'react-router-dom';
 
 function App() {
 
   let [shoes, shoes변경] = useState(dett);
+  
+
+  function 낮은가격순정렬() {
+    const copyArray = [...shoes];
+    const changedArray = copyArray.sort(function (a, b) {
+      return a.price - b.price
+    });
+    shoes변경(changedArray);
+  };
+
+  function 높은가격순정렬() {
+    const copyArray = [...shoes];
+    const changedArray = copyArray.sort(function (a, b) {
+      return b.price - a.price
+    });
+    shoes변경(changedArray);
+  };
+
+  function 기본정렬() {
+    shoes변경(dett);
+  };
 
   return (
     <div className="App">
@@ -19,8 +40,8 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link ><Link to="/">Home</Link></Nav.Link>
-              <Nav.Link ><Link to="/detail">Detail</Link></Nav.Link>
+              <Nav.Link as ={Link}to="/">Home</Nav.Link>
+              <Nav.Link as ={Link}to="/detail">Detail</Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -32,7 +53,6 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-
       <Switch>
         <Route exact path="/">
           <div className='jumbotron'>
@@ -50,42 +70,34 @@ function App() {
           <div className='container'>
             <div className='row'>
               {shoes.map((a, i) => { return (<상품 key={i} shoes={shoes[i]} />) })}
-
             </div>
-
+            <Button variant="dark" onClick={낮은가격순정렬}>낮은 가격순 </Button>{' '}
+            <Button variant="dark" onClick={높은가격순정렬}>높은 가격순</Button>{' '}
+            <Button variant="dark" onClick={기본정렬}>기본정렬</Button>{' '}
           </div>
-
         </Route>
         <Route path="/detail/:id">
-          <Detail shoes={shoes} />
+          <Detail shoes={shoes}  />
         </Route>
-
-
         {/* <Route path="/어떤경로" component={컴포넌트이름}></Route> */}
-
         <Route path="/:id">
           <div> 아무거나 적었을떄 이거 </div>
         </Route>
-
       </Switch>
-
-
     </div>
 
   );
 }
-
-function 상품(props) {
+function 상품(props) {const a = useHistory()
   return (
     <div className="col-md-4" >
-      <img src={props.shoes.src} width="100%" />
+      <img src={props.shoes.src}  width="100%" />
       <h4>{props.shoes.title}</h4>
       <p>{props.shoes.content}</p>
       <p>{props.shoes.price}</p>
+      <Button variant="primary"  onClick={()=>{ a.push(`/detail/${props.shoes.id}`)}} >자세히보기</Button>{' '}
     </div>
 
   );
 }
-
-
 export default App;
--- src/Detail.js
+++ src/Detail.js
@@ -1,32 +1,56 @@
-import React,{useState} from 'react';
-import {useHistory, useParams} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import './Detail.scss';
+let 박스 = styled.div`
+  padding : 20px;
+`;
 
-function Detail(props){
+let 제목 = styled.h4`
+  font-size : 25px;,
+  color : ${props => props.색상}
+`
 
-let history = useHistory();
-let { id  } = useParams();
+function Detail(props) {
+  let [매진스위치, 매진스위치변경] = useState(false);
+  useEffect(() => {
+    let 타이머 = setTimeout(()=>{매진스위치변경(true)},3000);
+  return ()=>{clearTimeout(타이머)}},[])
+    
+  let history = useHistory();
+  let { id } = useParams();
 
-let clikedId = props.shoes.find(function(product){
-  return product.id == id;
-});
+  let clikedId = props.shoes.find(function (product) {
+    return product.id == id;
+  });
+  let [인풋,인풋값] = useState('');
 
 
-    return(
-      <div className="container" key={clikedId.id} >
-        <div className="row">
-          <div className="col-md-6">
-            <img src={clikedId.src} width="100%" />
-          </div>
-          <div className="col-md-6 mt-4">
-            <h4 className="pt-5">{clikedId.title}</h4>
-            <p>{clikedId.content}</p>
-            <p>{clikedId.price}</p>
-            <button className="btn btn-danger">주문하기</button> 
-            <button className="btn btn-danger" onClick={()=>{ history.goBack();}}>이전페이지</button> 
-          </div>
+  return (
+    <div className="container" key={clikedId.id} >
+      <박스>
+        <제목 className='red'>상세페이지</제목>
+      </박스>
+      {인풋}
+      <input onChange={(e)=>{인풋값(e.target.value)}}/>
+      {매진스위치 === false ? <div className='my-alret2'>
+        <p>곧 매진</p>
+      </div> : null}
+
+      <div className="row">
+        <div className="col-md-6">
+          <img src={clikedId.src} width="100%" />
         </div>
-  </div> 
-    )
-  }
-  
-  export default Detail;
+        <div className="col-md-6 mt-4">
+          <h4 className="pt-5">{clikedId.title}</h4>
+          <p>{clikedId.content}</p>
+          <p>{clikedId.price}</p>
+          <button className="btn btn-danger">주문하기</button>
+          <button className="btn btn-danger" onClick={() => { history.goBack(); }}>이전페이지</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Detail;
